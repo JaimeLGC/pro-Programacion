426ecea205284f0ece65a82e5563514483180bbe
JaimeLGC <jaimeleongarcia123@gmail.com>
2023-03-08 18:37:55 +0000

    split_case

--- ut4/ejer/repaso_funciones/split_case.py
+++ ut4/ejer/repaso_funciones/split_case.py
@@ -3,6 +3,12 @@
 # *********************************
 
 
-def split_case():
-    # TU CÓDIGO AQUÍ
-
+def split_case(words: list) -> list:
+    lower_words = []
+    upper_words = []
+    for word in words:
+        if word.islower():
+            lower_words.append(word)
+        elif word.isupper():
+            upper_words.append(word)
+    return lower_words, upper_words
