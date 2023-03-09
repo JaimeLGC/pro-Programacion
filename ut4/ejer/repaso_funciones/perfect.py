6e0a44d252b7d85e760038097864d4336a0b9bf0
JaimeLGC <jaimeleongarcia123@gmail.com>
2023-03-09 18:19:24 +0000

    perfect.py

--- ut4/ejer/repaso_funciones/perfect.py
+++ ut4/ejer/repaso_funciones/perfect.py
@@ -3,6 +3,20 @@
 # *****************
 
 
-def is_perfect():
-    # TU CÓDIGO AQUÍ
+def is_perfect(n: int) -> bool:
+    divs = list(num for num in range(1, n) if n % num == 0)
+    if n == sum(divs):
+        return True
+    return False
 
+
+# def is_perfect(n: int) -> bool:
+# num = 1
+# divs = []
+# for num in range(1, n):
+#     if n % num == 0:
+#         divs.append(num)
+#     num += 1
+# if n == sum(divs):
+#     return True
+# return False
