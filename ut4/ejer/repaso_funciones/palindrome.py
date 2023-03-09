ee51a35e3a93b00cd29f0923961dfaa9b36abb71
JaimeLGC <jaimeleongarcia123@gmail.com>
2023-03-09 18:33:43 +0000

    prueba palindromo

---
+++ ut4/ejer/repaso_funciones/palindrome.py
@@ -0,0 +1,11 @@
+# **********
+# PALÍNDROMO
+# **********
+
+
+def is_palindrome(text: str) -> bool:
+    text_join = "".join(text.lower().split())
+    # backwards_text = text_join
+    # if text_join == backwards_text:
+    #     return True
+    return False
