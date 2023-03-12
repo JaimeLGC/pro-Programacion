aaaedeae8bdde7d3fa4eab2271e451a17a228f99
JaimeLGC <jaimeleongarcia123@gmail.com>
2023-03-12 21:03:03 +0000

    num_vowels

--- ut4/ejer/repaso_funciones/count_vowels.py
+++ ut4/ejer/repaso_funciones/count_vowels.py
@@ -3,6 +3,7 @@
 # *******************************
 
 
-# def count_vowels():
-    # TU CÓDIGO AQUÍ
-
+def count_vowels(text: str):
+    VOWELS = "aeiou"
+    num_vowels = len(list(char for char in text if char in VOWELS))
+    return num_vowels
