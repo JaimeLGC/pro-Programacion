1a68e7b213e01c09887e5f82b06bbe11c18e89b3
JaimeLGC <jaimeleongarcia123@gmail.com>
2023-03-12 23:25:36 +0000

    palindromo

--- ut4/ejer/repaso_funciones/palindrome.py
+++ ut4/ejer/repaso_funciones/palindrome.py
@@ -5,7 +5,7 @@
 
 def is_palindrome(text: str) -> bool:
     text_join = "".join(text.lower().split())
-    backwards_text = "".join(reversed(text.lower().split()))
+    backwards_text = "".join(reversed(list(text_join)))
     if text_join == backwards_text:
         return True
     return False
