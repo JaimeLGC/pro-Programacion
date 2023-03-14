b4b0ecec7e63f95ebbe25a939ad5366eb84e5d09
JaimeLGC <jaimeleongarcia123@gmail.com>
2023-03-14 16:21:53 +0000

    repaso general

--- ut4/ejer/repaso_funciones/magic_square.py
+++ ut4/ejer/repaso_funciones/magic_square.py
@@ -3,6 +3,11 @@
 # ***************
 
 
-def is_magic_square():
-    # TU CÓDIGO AQUÍ
+def is_magic_square(values: list):
+    target = sum(values[0])
+    num_ammount = len(values[0])
+    n = 0
+    items = []
 
+    for value in values:
+        items.append(value[n])
