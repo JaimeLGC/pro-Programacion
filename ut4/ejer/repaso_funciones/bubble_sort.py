2c273ba5106fd1d1f0911e027f6cfdf82a4856b4
JaimeLGC <jaimeleongarcia123@gmail.com>
2023-03-14 10:58:22 +0000

    consecutive_seq

---
+++ ut4/ejer/repaso_funciones/bubble_sort.py
@@ -0,0 +1,18 @@
+# **********************
+# ORDENANDO CON BURBUJAS
+# **********************
+
+
+def bubble(items):
+    citems = items.copy()
+    change = True
+    while change:
+        change = False
+        for i in range(len(citems) - 1):
+            current = citems[i]
+            next = citems[i + 1]
+            if current > next:
+                citems[i] = next
+                citems[i + 1] = current
+                change = True
+    return citems
---
+++ ut4/ejer/repaso_funciones/cycle_alphabet.py
@@ -0,0 +1,15 @@
+# *****************
+# ALFABETO CIRCULAR
+# *****************
+
+
+def run(max_letters: int) -> str:
+    text = ""
+    for i in range(0, max_letters + 1):
+        text + i
+
+    return text
+
+
+if __name__ == "__main__":
+    run(0)
