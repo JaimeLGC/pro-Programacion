533bd31402156d938dc5c3ae2aba6be1fa551046
JaimeLGC <jaimeleongarcia123@gmail.com>
2023-03-23 18:30:27 +0000

    Añadido mobile.py

--- ut5/ejer/mobile.py
+++ ut5/ejer/mobile.py
@@ -5,12 +5,17 @@ class MobilePhone:
         self.num_cores = num_cores
         self.apps = []
         self.status = False
+        self.power = 100
 
-    def power_on(self):
-        self.status = True
+    def power_drain(self):
+        if self.manufacturer == 'Xiaomi':
+            self.power -= 2
 
-    def power_off(self):
-        self.status = False
+    def switch(self):
+        if self.status == True:  
+            self.status = False
+        elif self.status == False:      
+            self.status = True
 
     def install_app(self, app: str):
         self.apps.append(app)
@@ -18,6 +23,12 @@ class MobilePhone:
     def uninstall_app(self, app: str):
         self.apps.remove(app)
 
+    def power_warning(self):
+        if self.power == 20:
+            print('Aviso, batería baja')
+        elif self.power == 10:
+            print('Aviso, batería MUY baja')
+        elif self.power == 0:
 
 mobile1 = MobilePhone("Xiaomi", 6.78, 4)
 
