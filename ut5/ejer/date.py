2903ef213286b5f707ddfd40a8ea0a0966776098
JaimeLGC <jaimeleongarcia123@gmail.com>
2023-04-18 19:55:54 +0100

    correcciones date.py

--- ut5/ejer/date.py
+++ ut5/ejer/date.py
@@ -1,21 +1,43 @@
-LONG_MONTHS = {
-    "January": 1,
-    "March": 3,
-    "May": 5,
-    "July": 7,
-    "August": 8,
-    "October": 10,
-    "December": 12,
+from __future__ import annotations
+
+weekdays = {
+    0: "DOMINGO",
+    1: "LUNES",
+    2: "MARTES",
+    3: "MIÉRCOLES",
+    4: "JUEVES",
+    5: "VIERNES",
+    6: "SÁBADO",
 }
-SHORT_MONTHS = {"April": 4, "June": 6, "September": 9, "November": 11}
+
+MONTHS = {
+    1: "ENERO",
+    2: "FEBRERO",
+    3: "MARZO",
+    4: "ABRIL",
+    5: "MAYO",
+    6: "JUNIO",
+    7: "JULIO",
+    8: "AGOSTO",
+    9: "SEPTIEMBRE",
+    10: "OCTUBRE",
+    11: "NOVIEMBRE",
+    12: "DICIEMBRE",
+}
+LONG_MONTHS = [1, 3, 5, 7, 8, 10, 12]
+SHORT_MONTHS = [4, 6, 9, 11]
 FEBRUARY = 2
 
 
 class Date:
     def __init__(self, day: int, month: int, year: int):
-
+        """Validar día, mes y año. Se comprobará si la fecha es correcta
+        (entre el 1-1-1900 y el 31-12-2050); si el día no es correcto, lo pondrá a 1;
+        si el mes no es correcto, lo pondrá a 1; y si el año no es correcto, lo pondrá a 1900.
+        Ojo con los años bisiestos.
+        """
         self.year = year
-        if 1990 <= year <= 2050:
+        if 1900 <= year <= 2050:
             self.year = year
         else:
             self.year = 1900
@@ -23,65 +45,139 @@ class Date:
             self.month = month
         else:
             self.month = 1
-        if 0 < day <= self.days_in_month(month, year):
+        if 0 < day <= self.days_in_month:
             self.day = day
         else:
             self.day = 1
 
-        """Validar día, mes y año. Se comprobará si la fecha es correcta
-        (entre el 1-1-1900 y el 31-12-2050); si el día no es correcto, lo pondrá a 1;
-        si el mes no es correcto, lo pondrá a 1; y si el año no es correcto, lo pondrá a 1900.
-        Ojo con los años bisiestos.
-        """
-
-    def is_leap_year(self, year: int) -> bool:
+    @staticmethod
+    def is_leap_year(year: int) -> bool:
         if year % 400 == 0:
             return True
-        if year % 4 == 0 and year % 100 == 0:
+        if year % 4 == 0 and year % 100 != 0:
             return True
         else:
             return False
 
-    def days_in_month(self, month, year) -> int:
-        if month in LONG_MONTHS.values():
+    @property
+    def days_in_month(self) -> int:
+        if self.month in LONG_MONTHS:
             return 31
-        if month in SHORT_MONTHS.values():
+        if self.month in SHORT_MONTHS:
             return 30
-        if month == 2:
-            if self.is_leap_year(year):
+        if self.month == 2:
+            if self.is_leap_year(self.year):
                 return 29
             return 28
 
-    def delta_days(self) -> int:
+    def get_delta_days(self) -> int:
         """Número de días transcurridos desde el 1-1-1900 hasta la fecha"""
-        
+        past_days = 0
+        for y in range(1900, self.year):
+            if self.is_leap_year(y) == True:
+                past_days += 1
+            past_days += 365
+        for m in range(self.month):
+            if m in LONG_MONTHS:
+                past_days += 31
+            if m in SHORT_MONTHS:
+                past_days += 30
+            if m == 2:
+                if self.is_leap_year(self.year):
+                    past_days += 29
+                else:
+                    past_days += 28
+        past_days += self.day - 1
+        return past_days
 
-    def weekday(self) -> int:
+    @property
+    def weekday(self) -> str:
         """día de la semana de la fecha (0 para domingo, ..., 6 para sábado).
         El 1-1-1900 fue domingo."""
-        pass
-        return 1
+        weekday_index = 1
+        delta_counter = 1
+        while delta_counter <= self.get_delta_days():
+            delta_counter += 1
+            if weekday_index == 6:
+                weekday_index = 0
+            else:
+                weekday_index += 1
+        return weekday_index
 
+    @property
     def is_weekend(self) -> bool:
-        pass
-        return True
+        weekend = [5, 6, 0]
+        if self.weekday in weekend:
+            return True
+        else:
+            return False
 
+    @property
     def short_date(self) -> str:
         """02/09/2003"""
-        pass
-        return "1"
+        day = self.day
+        month = self.month
+        if self.day <= 9:
+            day = f"0{self.day}"
+        if self.month <= 9:
+            month = f"0{self.month}"
+        return f"{day}/{month}/{self.year}"
 
     def __str__(self):
         """martes 2 de septiembre de 2003"""
-        pass
-        return 1
+        day = weekdays[self.weekday]
+        month = MONTHS[self.month]
+        return f"{day} {self.day} DE {month} DE {self.year}"
 
-    # operador + suma días a la fecha
-    # operador - resta días a la fecha o calcula la diferencia entre dos fechas
-    # operador == dice si dos fechas son iguales
-    # operador > dice si una fecha es mayor que otra
-    # operador < dice si una fecha es menor que otra
+    def __add__(self, days: int) -> Date:
+        """Sumar un número de días a la fecha"""
+        ...
+
+    def __sub__(self, other: Date | int) -> int | Date:
+        """Dos opciones:
+        1) Restar una fecha a otra fecha -> Número de días
+        2) Restar un número de días la fecha -> Nueva fecha"""
+        ...
+
+    def __gt__(self, date: Date) -> bool:
+        if self.year > date.year:
+            return True
+        if self.year < date.year:
+            return False
+        else:
+            if self.month > date.month:
+                return True
+            if self.month < date.month:
+                return False
+            else:
+                return self.day > date.day
+
+    def __lt__(self, date: Date) -> bool:
+        if date.year > self.year:
+            return True
+        if date.year < self.year:
+            return False
+        else:
+            if date.month > self.month:
+                return True
+            if date.month < self.month:
+                return False
+            else:
+                return date.day > self.day
+
+    def __eq__(self, other: Date) -> bool:
+        if self > other:
+            return False
+        if self < other:
+            return False
+        else:
+            return True
 
 
-fecha1 = Date(29, 3, 1899)
-print(fecha1.day, fecha1.month, fecha1.year)
+fecha1 = Date(8, 4, 2023)
+fecha2 = Date(2, 4, 2023)
+dias = fecha1.get_delta_days
+diasemana = fecha1.weekday
+isweekend = fecha1.is_weekend
+sdate = fecha1.short_date
+print(diasemana)
