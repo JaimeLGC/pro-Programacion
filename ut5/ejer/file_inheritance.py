0adf0a8424af281428f0f40616b662221bf096c9
JaimeLGC <jaimeleongarcia123@gmail.com>
2023-04-21 16:36:06 +0100

    correcciones

--- ut5/ejer/file_inheritance.py
+++ ut5/ejer/file_inheritance.py
@@ -1,20 +1,19 @@
 class File:
     def __init__(self, path: str):
         self.path = path
-        self.content = []
+        self.contents = []
 
-    def add_content(self, content: str):
-        self.content.append(content)
+    def add_content(self, content: str) -> None:
+        self.contents.append(content)
 
     @property
-    def size(self):
+    def size(self) -> int:
         size = 0
-        for item in self.content:
+        for item in self.contents:
             size += len(item)
         return size
 
-    @property
-    def info(self):
+    def info(self) -> str:
         print(vars(self))
 
 
@@ -33,21 +32,15 @@ class VideoFile(MediaFile):
         super().__init__(path, codec, geoloc, duration)
         self.dimensions = dimensions
 
-    @property
-    def info(self):
+    def info(self) -> str:
         print(
             f"{self.path} [size={self.size}B]\nCodec: {self.codec}\nGeolocalization: {self.geoloc}\nDuration: {self.duration}s\nDimensions: {self.dimensions}"
         )
 
-
-file1 = File("abcd")
-file1.add_content("fdfd")
-file1.add_content("fdf")
-file1.add_content("fd")
 
 file2 = VideoFile(
     "/home/python/vanrossum.mp4", "h264", (23.5454, 31.4343), 487, (1920, 1080)
 )
 file2.add_content("audio/ogg")
 file2.add_content("video/webm")
-file2.info
+file2.info()
