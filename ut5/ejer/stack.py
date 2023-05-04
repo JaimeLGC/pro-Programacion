cd3629e9d1d663c3b9182c1a01d433764c797762
JaimeLGC <jaimeleongarcia123@gmail.com>
2023-05-04 19:29:02 +0100

    prueba

--- ut5/ejer/stack.py
+++ ut5/ejer/stack.py
@@ -3,60 +3,68 @@ from __future__ import annotations
 
 class IntegerStack:
     def __init__(self, *, max_size: int = 10):
-        ...
+        self.max_size = max_size
+        self.items = []
 
     def push(self, item: int) -> bool:
-        '''Si la pila está llena retornar False, en otro caso retornar True'''
-        ...
+        if len(self.items) < self.max_size:
+            self.items.insert(0, item)
+            return True
+        else:
+            return False
 
     def pop(self) -> int:
-        '''Extraer el elemento que está en el TOP de la pila'''
-        ...
+        if self.items[0]:
+            result = self.items[0]
+            self.items.pop(0)
+        return result
 
     def top(self) -> int:
-        '''Devolver el elemento que está en el TOP de la pila (sin extracción)'''
-        ...
+        if self.items[0]:
+            return self.items[0]
 
     def is_empty(self) -> bool:
-        '''Indica si la pila está vacía'''
-        ...
+        if not self.items:
+            return True
+        return False
 
     def is_full(self) -> bool:
-        '''Indica si la pila está llena -> max_size'''
-        ...
+        if len(self.items) == self.max_size:
+            return True
+        return False
 
     def expand(self, factor: int = 2) -> None:
-        '''Expande el tamaño máximo de la pila en el factor indicado'''
-        ...
+        self.max_size = self.max_size * factor
 
     def dump_to_file(self, path: str) -> None:
-        '''Vuelca la pila a un fichero. Cada item en una línea'''
-        ...
+        with open(path, "w") as f:
+            for item in self.items:
+                f.write(f"{item}\n")
 
     @classmethod
     def load_from_file(cls, path: str) -> IntegerStack:
-        '''Crea una pila desde un fichero. Si la pila se llena al ir añadiendo elementos
-        habrá que expandir con los valores por defecto'''
+        """Crea una pila desde un fichero. Si la pila se llena al ir añadiendo elementos
+        habrá que expandir con los valores por defecto"""
         ...
 
     def __getitem__(self, index: int) -> int:
-        '''Devuelve el elemento de la pila en el índice indicado'''
+        """Devuelve el elemento de la pila en el índice indicado"""
         ...
 
     def __setitem__(self, index: int, item: int) -> None:
-        '''Establece el valor de un elemento de la pila mediante el índice indicado'''
+        """Establece el valor de un elemento de la pila mediante el índice indicado"""
         ...
 
     def __len__(self):
-        '''Número de elementos que contiene la pila'''
+        """Número de elementos que contiene la pila"""
         ...
 
     def __str__(self):
-        '''Cada elemento en una línea distinta empezando por el TOP de la pila'''
+        return "\n".join(list(str(item for item in self.items)))
         ...
 
     def __add__(self, other: IntegerStack) -> IntegerStack:
-        '''La segunda pila va "encima" de la primera'''
+        """La segunda pila va "encima" de la primera"""
         ...
 
     def __iter__(self) -> IntegerStackIterator:
