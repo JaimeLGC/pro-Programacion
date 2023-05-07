8e042be695f296b92434f21a3e9dc3bb85920712
JaimeLGC <jaimeleongarcia123@gmail.com>
2023-05-07 22:02:22 +0100

    a

--- ut5/ejer/stack.py
+++ ut5/ejer/stack.py
@@ -45,19 +45,27 @@ class IntegerStack:
     def load_from_file(cls, path: str) -> IntegerStack:
         """Crea una pila desde un fichero. Si la pila se llena al ir añadiendo elementos
         habrá que expandir con los valores por defecto"""
-        ...
+        with open(path, "r") as f:
+            stack = IntegerStack(max_size=len(f.readlines()))
+            stack.items = f.readlines()
+            return stack
+
+    load_from_file
 
     def __getitem__(self, index: int) -> int:
         """Devuelve el elemento de la pila en el índice indicado"""
-        ...
+        return self.items[index]
 
     def __setitem__(self, index: int, item: int) -> None:
         """Establece el valor de un elemento de la pila mediante el índice indicado"""
-        ...
+        if index > len(self.items):
+            self.items.append(item)
+        else:
+            self.items[index] = item
 
     def __len__(self):
         """Número de elementos que contiene la pila"""
-        ...
+        return len(self.items)
 
     def __str__(self):
         return "\n".join(list(str(item for item in self.items)))
@@ -65,7 +73,9 @@ class IntegerStack:
 
     def __add__(self, other: IntegerStack) -> IntegerStack:
         """La segunda pila va "encima" de la primera"""
-        ...
+        result = IntegerStack(max_size=(len(self.items) + len(other.items)))
+        result.items += other.items
+        result.items += self.items
 
     def __iter__(self) -> IntegerStackIterator:
         ...
